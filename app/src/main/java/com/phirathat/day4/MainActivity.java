93a2cb263f26d4a8dde6147611a43f058c32edf3
phgoff <mous17zy@hotmail.com>
2018-03-23 12:02:38 +0700

    edit CustomView

--- app/src/main/java/com/phirathat/day4/MainActivity.java
+++ app/src/main/java/com/phirathat/day4/MainActivity.java
@@ -29,18 +29,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int[] resId = { R.drawable.aerithgainsborough
-                , R.drawable.barretwallace, R.drawable.caitsith
-                , R.drawable.cidhighwind, R.drawable.cloudstrife
-                 };
-
-        String[] list = { "Aerith Gainsborough", "Barret Wallace", "Cait Sith"
-                , "Cid Highwind", "Cloud Strife", "RedXIII", "Sephiroth"
-                , "Tifa Lockhart", "Vincent Valentine", "Yuffie Kisaragi"
-                , "ZackFair" };
-
-        CustomAdapter adapter = new CustomAdapter(getApplicationContext(), list, resId);
-
 
         final TextView tv1 = findViewById(R.id.tv1);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -72,6 +60,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Custom ListView
+        int[] resId = { R.drawable.aerithgainsborough
+                , R.drawable.barretwallace, R.drawable.caitsith
+                , R.drawable.cidhighwind, R.drawable.cloudstrife
+        };
+
+        String[] list = { "Aerith Gainsborough", "Barret Wallace", "Cait Sith"
+                , "Cid Highwind", "Cloud Strife", "RedXIII", "Sephiroth"
+                , "Tifa Lockhart", "Vincent Valentine", "Yuffie Kisaragi"
+                , "ZackFair" };
+        CustomAdapter customadapter = new CustomAdapter(getApplicationContext(), list, resId);
+        ListView listView = (ListView)findViewById(R.id.listView1);
+        listView.setAdapter(customadapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long row_id) {
+                TextView textView = (TextView)view.findViewById(R.id.textView1);
+                System.out.println( textView.getText());
+                Toast.makeText(getApplicationContext(), "Clicked on item:" +  position , Toast.LENGTH_SHORT).show();
+            }
+      });
     }
 
     @Override
